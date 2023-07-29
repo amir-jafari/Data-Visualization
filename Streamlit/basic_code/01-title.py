3b03f4cb60c4840669a6bbdd4508173c29d73afb
amir-jafari <amir.h.jafari@okstate.edu>
2023-07-28 21:40:06 -0400

    Add header and write streamlit

---
+++ Streamlit/basic_code/01-title.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+
+st.title("Streamlit App")
+st.markdown("""___""")
+st.header("Header 1")
+st.subheader("header2")
+st.divider()
+st.write('This is the place you can start writing')
+st.divider()
+code = """df = pd.DataFrame(
+    np.random.randint(low=0, high=100, size=(10, 10)),
+    columns=('col %d' % i for i in range(10)))
+
+st.dataframe(df)"""
+st.code(code, language="python")
